70d25341b19e5df56bf7a97387d765edfee18983
sathvik125 <sathvikreddykasarla1@gmail.com>
2022-12-05 12:57:42 +0530

    pushedd

---
+++ src/main/java/mavenjava/mavenjavaproject/App.java
@@ -0,0 +1,13 @@
+package mavenjava.mavenjavaproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! from sathvik " );
+    }
+}
